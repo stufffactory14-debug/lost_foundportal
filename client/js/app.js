39999ca540741424bc55fed2d2e5f09d17b85f61
stufffactory14-debug <stufffactory14@gmail.com>
2026-03-26 12:51:27 +0530

    make changes in app.js

--- client/js/app.js
+++ client/js/app.js
@@ -1,19 +1,16 @@
 const form = document.getElementById("uploadForm");
 
 if (form) {
-
     form.addEventListener("submit", async (e) => {
-
         e.preventDefault();
 
         const formData = new FormData(form);
-
         const type = formData.get("type");
 
-        console.log("Item Type:", type); // debug
+        console.log("Item Type:", type);
 
         try {
-            const res = await fetch("http://localhost:5000/api/items/create", {
+            const res = await fetch(`${process.env.REACT_APP_API_URL}/api/items/create`, {
                 method: "POST",
                 body: formData
             });
@@ -21,14 +18,11 @@ if (form) {
             const data = await res.json();
 
             alert(data.message);
-
             form.reset();
 
         } catch (error) {
             console.error(error);
             alert("Error submitting item");
         }
-
     });
-
 }
